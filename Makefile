LineageOS/android_kernel_nvidia_linux-4.9_hardware_nvidia_platform_t19x_jakku_kernel-dts

Add android dts
Change-Id: I948c2488a7e9a75218446ef560253d7bf8177aef

Makefile
@@ -5,6 +5,7 @@ dtbo-y :=
 makefile-path := platform/t19x/jakku/kernel-dts
 
 dtb-$(CONFIG_ARCH_TEGRA_19x_SOC) += tegra194-p3668-all-p3509-0000.dtb
+dtb-$(CONFIG_ARCH_TEGRA_19x_SOC) += tegra194-p3668-all-p3509-0000-android.dtb
 dtbo-$(CONFIG_ARCH_TEGRA_19x_SOC) += tegra194-p3668-all-p3509-0000-hdr40.dtbo
 dtbo-$(CONFIG_ARCH_TEGRA_19x_SOC) += tegra194-p3668-all-p3509-0000-adafruit-sph0645lm4h.dtbo
 dtbo-$(CONFIG_ARCH_TEGRA_19x_SOC) += tegra194-p3668-all-p3509-0000-fe-pi-audio-z-v2.dtbo

LineageOS/android_hardware_nvidia_platform_t19x_jakku_kernel-dts

Change-Id: I2af2d5e215631c637736bcc5b46ccafac2f601d0

Makefile
@@ -10,6 +10,7 @@ BUILD_ENABLE=y
 endif
 
 dtb-$(BUILD_ENABLE) += tegra194-p3668-all-p3509-0000.dtb
+dtb-$(BUILD_ENABLE) += tegra194-p3668-all-p3509-0000-android.dtb
 dtb-$(BUILD_ENABLE) += tegra194-p3668-0000-p3509-0000.dtb
 dtb-$(BUILD_ENABLE) += tegra194-p3668-0001-p3509-0000.dtb
 dtb-$(CONFIG_ARCH_TEGRA_19x_SOC) += tegra194-p3668-all-p3509-0000-kexec.dtb